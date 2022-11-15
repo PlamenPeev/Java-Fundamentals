b0b3acbaae99a8994a52c8266126f2e128c7bae7
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2022-11-15 17:02:16 +0200

    Add files via upload

---
+++ Methods/Methods_lab/P02Grades.java
@@ -0,0 +1,31 @@
+package lab;
+
+import java.util.Scanner;
+
+public class P02Grades {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double grade = Double.parseDouble(scanner.nextLine());
+        printWordOfGrade(grade);
+    }
+
+    private static void printWordOfGrade(double grade) {
+//	2.00 – 2.99 - "Fail"
+//	3.00 – 3.49 - "Poor"
+//	3.50 – 4.49 - "Good"
+//	4.50 – 5.49 - "Very good"
+//	5.50 – 6.00 - "Excellent"
+        if( grade >= 2.00 && grade <= 2.99){
+            System.out.println("Fail");
+        }else if( grade >= 3.00 && grade <= 3.49){
+            System.out.println("Poor");
+        }else if( grade >= 3.50 && grade <= 4.49){
+            System.out.println("Good");
+        }else if( grade >= 4.50 && grade <= 5.49){
+            System.out.println("Very good");
+        }else if( grade >= 5.50 && grade <= 6.00){
+            System.out.println("Excellent");
+        }
+
+    }
+}
